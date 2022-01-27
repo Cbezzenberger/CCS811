4fbdc9463370c1dfe06746acc1f3d1f3872b4e56
Cbezzenberger <carl.github@outlook.com>
2022-01-27 23:59:15 +0100

    whoops

--- sensorDataGen.py
+++ sensorDataGen.py
@@ -3,12 +3,12 @@ import csv
 import time
 # from collections import deque
 import os
-from sys import exit
+import sys
 
 ccs = Adafruit_CCS811()
 
 fieldnames = ["Timestamp", "Co2", "Tvoc"]
-Error_counter = []
+IOerror_counter = []
 
 def update_ccsdata():
     ccs.readData()
@@ -24,18 +24,27 @@ def write_csvfile():
 
     while True:
         csvpath = f"csvfiles/{time.strftime('%H')}_ccsdata.csv"
+
         if not os.path.exists(csvpath):
             with open(csvpath, "w")as csv_file:
                 csv.DictWriter(csv_file, fieldnames=fieldnames).writeheader()
-        Try:
+
+        try:
             with open(csvpath, "a") as csv_file:
                 csvwriter = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
                 info = update_ccsdata()
 
                 csvwriter.writerow(info)
-                # print(ts, CO2, TVOC)
                 #TODO: Create delete function for old CSVs
+                IOerror_counter = 0 #reset IOError counter after successful write attempt.
             time.sleep(1)
-        
+
+        except IOError: #Seems to just happen occasionally
+            if IOerror_counter > 3:
+                sys.exit()
+            else:
+                time.sleep(10)
+                IOerror_counter += 1
+                
 write_csvfile()
