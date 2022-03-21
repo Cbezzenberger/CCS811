8602c29076309194139436ae0844f20c5528efe9
Cbezzenberger <carl.github@outlook.com>
2022-03-21 16:25:51 +0100

    Introducing multiprocessing and scheduling of deletion function

--- sensorDataGen.py
+++ sensorDataGen.py
@@ -1,21 +1,34 @@
+from datetime import datetime, timedelta
+import multiprocessing
 from Adafruit_CCS811 import Adafruit_CCS811
+from multiprocessing import Process
 import csv
 import time
 import os
 import sys
+import schedule
+#TODO: Switch from csv-based to tinydb in-memory storage.
+#from tinydb.storages import MemoryStorage
 
 ccs = Adafruit_CCS811()
 
 fieldnames = ["Timestamp", "Co2", "Tvoc"]
-IOerror_counter = []
+IOerror_counter = 0
+
+#db = TinyDB(storage=MemoryStorage)
 
 def update_ccsdata():
     ccs.readData()
-    return {
-        "Timestamp": time.strftime("%H:%M:%S"),
-        "Co2": ccs.geteCO2(),
-        "Tvoc": ccs.getTVOC()
-        }
+    if ccs.geteCO2() < 7500 and ccs.getTVOC() < 7500:
+        return {
+            "Timestamp": time.strftime("%H:%M:%S"),
+            "Co2": ccs.geteCO2(),
+            "Tvoc": ccs.getTVOC()}
+    else:
+        return {
+            "Timestamp": time.strftime("%H:%M:%S"),
+            "Co2": 0,
+            "Tvoc": 0}
 
 def write_csvfile(update_frq = 5): #define update frequency in seconds. This will create a new csvfile etry according to the set frequency.
     if not os.path.exists("csvfiles"): #Create subdirectory for csvfiles if it does not exist.
@@ -37,6 +50,7 @@ def write_csvfile(update_frq = 5): #define update frequency in seconds. This wil
 
                 csvwriter.writerow(info)
                 #TODO: Create delete function for old CSVs
+                # csvfiles = [i for i in os.listdir("./csvfiles") if os.path.isfile(os.path.join("csvfiles, f"))]
                 IOerror_counter = 0 #reset IOError counter after successful write attempt.
             time.sleep(update_frq)
 
@@ -47,4 +61,25 @@ def write_csvfile(update_frq = 5): #define update frequency in seconds. This wil
                 time.sleep(10)
                 IOerror_counter += 1
                 
-write_csvfile()
+#Function to delete the oldest CSVfile, once there are more than 27 csvfiles in the folder.
+#TODO:Section should be updated, finding and deleting a specific date offset file instead of just the oldest.
+
+
+def delete_old_csv(): #Thanks @RightmireM
+    schedule.every().day.at("23:55").do(_delete_old_csv)
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
+
+def _delete_old_csv():
+    tmrw = datetime.today() + timedelta(days=1)
+    os.remove(os.path.abspath(f"csvfiles/{tmrw}_ccsdata.csv"))
+
+#TODO:Not sure if this is the most efficient implementation.
+if __name__ == '__main__':
+    p1 = multiprocessing.Process(target=delete_old_csv)
+    p1.start()
+    p2 = multiprocessing.Process(target=write_csvfile)
+    p2.start()
+    p1.join()
+    p2.join()
--- sensorPlot.py
+++ sensorPlot.py
@@ -1,22 +1,26 @@
 import time
 import pandas as pd
-# import glob
 import os
 from collections import deque
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib.dates as mdates
 
-Ts = deque(maxlen=360)
-Co2 = deque(maxlen=360)
-Tvoc = deque(maxlen=360)
+# Ts = deque(maxlen=360)
+# Co2 = deque(maxlen=360)
+# Tvoc = deque(maxlen=360)
 
 def animate_sensor_data(i):
     current_day = time.strftime("%d")
     sensor_data = pd.read_csv(f"csvfiles/{current_day}_ccsdata.csv")
     
-    Ts.append(time.strptime(sensor_data["Timestamp"].iat[-1], "%H:%M:%S"))
-    Co2.append(sensor_data["Co2"].iat[-1])
-    Tvoc.append(sensor_data["Tvoc"].iat[-1])
+    # Ts.append(time.strptime(sensor_data["Timestamp"].iat[-1], "%H:%M:%S"))
+    # Co2.append(sensor_data["Co2"].iat[-1])
+    # Tvoc.append(sensor_data["Tvoc"].iat[-1])
+
+    Ts = sensor_data['Timestamp'][-360:]
+    Co2 = sensor_data['Co2'][-360:]
+    Tvoc = sensor_data['Tvoc'][-360:]
 
     plt.cla()
 
@@ -29,20 +33,3 @@ def animate_sensor_data(i):
 ani = animation.FuncAnimation(plt.gcf(), animate_sensor_data, interval = 5000)
 
 plt.show()
-
-# def get_file_ctime(fp):
-#     return os.path.getctime(fp)
-# file_list = glob.glob("csvfiles/*")
-# file_list.sort(key = get_file_ctime)
-
-# #get 3600 second entries from last and second to last file
-# file_currenth = file_list[:0]
-# file_lasth = file_list[:-1]
-
-# sensordata1h = deque(maxlen=3600)
-# while True:
-#     if file_currenth = 
-#     with open(file_currenth, "r") as f:
-#         with open(file_lasth, "r") as lf:
-#             sensordata1h = 
-# %%
