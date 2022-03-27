49a65e7a812829b96be4cf23ffb45f89edc7989f
Cbezzenberger <carl.github@outlook.com>
2022-03-28 00:37:26 +0200

    Addition of Epaper Display function

---
+++ sensorEpaperPlot.py
@@ -0,0 +1,47 @@
+#First clone geeekpi/epaper
+import time
+from Epaper import *
+from PIL import Image
+import sys
+
+def print2epaper(imgfile):
+    fnf_counter = 0 #FileNotFound Counter
+    X_PIXEL = 128
+    Y_PIXEL = 250
+    
+    try:
+        e = Epaper(X_PIXEL, Y_PIXEL)
+        
+        f = Image.open(imgfile)
+        fnf_counter = 0
+        
+        rBuf = [0] * 4000
+        bBuf = [0] * 4000
+
+        f = f.convert('RGB')
+        data = f.load()
+        for y in range(250):
+            for x in range(128):
+                if data[x,y] == (237,28,36): #IndexError: image index out of range TODO: Probably resize image to correct resolution.
+                    index = int(16 * y + (15 - (x - 7) / 8))
+                    tmp = rBuf[index]
+                    rBuf[index] = tmp | (1 << (x % 8))
+                elif data[x,y] == (255,255,255):
+                    index = int(16 * y + (15 - (x - 7) / 8))
+                    tmp = bBuf[index]
+                    bBuf[index] = tmp | (1 << (x % 8))
+    
+        e.flash_red(buf = rBuf)
+        e.flash_black(buf=bBuf)
+
+    except FileNotFoundError:
+        if fnf_counter <= 5:
+            time.sleep(2)
+            print(f"No png file found to print to epaper display. Retrying {fnf_counter}")
+            fnf_counter += 1
+        elif fnf_counter > 5:
+            sys.exit("No png file found to print to epaper display. Exiting...\n")
+
+while True:
+    print2epaper("fig.png")
+    time.sleep(5)
--- sensorPlot.py
+++ sensorPlot.py
@@ -1,35 +1,48 @@
-import time
+from datetime import datetime
 import pandas as pd
-import os
-from collections import deque
+from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import matplotlib.dates as mdates
+import matplotlib.dates as md
+import matplotlib.ticker as tkr
 
-# Ts = deque(maxlen=360)
-# Co2 = deque(maxlen=360)
-# Tvoc = deque(maxlen=360)
+cm = 1/2.54 #centimeters to inches conversion
+fig, ax = plt.subplots(figsize = (2.38*cm,4.88*cm), dpi = 130) #current display is 250×122@2.13".
+    #Trying to get to exact pixels for later conversion to ePaper.
+# fig, ax = plt.subplots()
+Ts, Co2 = [],[]
+ln, = plt.plot([], [], 'r-')
+ln2, = plt.plot([], [], 'k--')
+plt.grid(which = 'both', axis='y', color = 'k', linestyle = ':', linewidth = '0.5')
 
-def animate_sensor_data(i):
-    current_day = time.strftime("%d")
-    sensor_data = pd.read_csv(f"csvfiles/{current_day}_ccsdata.csv", parse_dates=True)
-    
-    # Ts.append(time.strptime(sensor_data["Timestamp"].iat[-1], "%H:%M:%S"))
-    # Co2.append(sensor_data["Co2"].iat[-1])
-    # Tvoc.append(sensor_data["Tvoc"].iat[-1])
+def plot_init():
+    ax.set_ylim(0,4000)
+    ax.yaxis.set_major_formatter(tkr.FuncFormatter(lambda y,pos: f"{int(y/1000)}k"))
+    return ln, ln2
 
-    Ts = sensor_data['Timestamp'][-360:]
-    Co2 = sensor_data['Co2'][-360:]
-    Tvoc = sensor_data['Tvoc'][-360:]
+def plot_update(frame):
+    current_day = datetime.utcnow().strftime("%d")
+    sensor_data = pd.read_csv(f"csvfiles/{current_day}_ccsdata.csv", parse_dates=['Timestamp'])
 
-    plt.cla()
+    Ts = sensor_data['Timestamp'][-240:]
+    Co2 = sensor_data['Co2'][-240:]
+    Tvoc = sensor_data['Tvoc'][-240:]
 
-    plt.plot(Ts, Co2, label = "eCO2")
-    plt.plot(Ts, Tvoc, label = "TVOC")
+    if frame == 0: #workaround for a bug in pyplot where axis updates don't work with blitting enabled
+        fig.canvas.resize_event()
+        
+    ax.set_ylim(0,Co2.max()+500)
+    ax.set_xlim(Ts.min(), Ts.max())
+    ln.set_data(Ts, Co2)
+    ln2.set_data(Ts, Tvoc)
+    xformatter = md.DateFormatter('%H:%M')
+    xlocator = md.MinuteLocator(byminute = [0,15,30,45], interval = 1)
+    ax.xaxis.set_major_locator(xlocator)
+    ax.xaxis.set_major_formatter(xformatter)
+    fig.autofmt_xdate()
 
-    plt.legend(loc = 'upper left')
-    plt.tight_layout()
+    plt.savefig('fig.png', bbox_inches = 'tight')
 
-ani = animation.FuncAnimation(plt.gcf(), animate_sensor_data, interval = 5000)
+    return ln, ln2
 
+ani = FuncAnimation(fig, plot_update, interval = 5000, init_func = plot_init, blit = True, repeat = False)
 plt.show()
