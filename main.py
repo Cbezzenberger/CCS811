935a8e7b033d200bf14cb4d999a50594e714545e
Cbezzenberger <carl.github@outlook.com>
2022-06-13 22:58:36 +0200

    Added multiprocessing to main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
+#! /usr/bin/env python3
+
 from time import time
 import sensorDataGen
 import sensorPlotEpaper
 import printToEpaper
 from time import sleep
+from multiprocessing import Process
 
-sleep(1)
-sensorDataGen.main()
-sleep(5)
-sensorPlotEpaper.main()
-sleep(5)
-printToEpaper.main()
-print("Running")
+#Create individual processes for each of the functions sensorDataGen.main(), sensorPlotEpaper.main(), and printToEpaper.main()
+
+def main():
+    p1 = Process(target=sensorDataGen.main)
+    p1.start()
+    p2 = Process(target=sensorPlotEpaper.main)
+    p2.start()
+    p3 = Process(target=printToEpaper.main)
+    p3.start()
+    p1.join()
+    p2.join()
+    p3.join()
+
+if __name__ == "__main__":
+    main()
