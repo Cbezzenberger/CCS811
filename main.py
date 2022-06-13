9fd73049ef14e86f069a69b3b3f76b0cc4aded09
Cbezzenberger <carl.github@outlook.com>
2022-06-13 22:14:28 +0200

    updated main to serve as central launch point

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from time import time
 import sensorDataGen
 import sensorPlotEpaper
 import printToEpaper
+from time import sleep
+
+sleep(1)
+sensorDataGen.main()
+sleep(5)
+sensorPlotEpaper.main()
+sleep(5)
+printToEpaper.main()
+print("Running")
