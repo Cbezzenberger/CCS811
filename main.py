8aeef1e4fd77c5de761fb9402b2364dd7ab04595
Cbezzenberger <carl.github@outlook.com>
2022-06-13 23:29:13 +0200

    pause between functions to solve sync issue at startup

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 #! /usr/bin/env python3
 
-from time import time
 import sensorDataGen
 import sensorPlotEpaper
 import printToEpaper
 from time import sleep
 from multiprocessing import Process
 
-#Create individual processes for each of the functions sensorDataGen.main(), sensorPlotEpaper.main(), and printToEpaper.main()
-
 def main():
     p1 = Process(target=sensorDataGen.main)
     p1.start()
+    sleep(10)
     p2 = Process(target=sensorPlotEpaper.main)
     p2.start()
+    sleep(10)
     p3 = Process(target=printToEpaper.main)
     p3.start()
     p1.join()
