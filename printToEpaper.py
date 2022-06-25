1ab50cba49fec275c4c343fe87c77697f76672c9
Cbezzenberger <carl.github@outlook.com>
2022-06-26 01:12:08 +0200

    First try to fix OSError: Too many open files

--- printToEpaper.py
+++ printToEpaper.py
@@ -1,8 +1,10 @@
 #First clone geeekpi/epaper
+from cmath import exp
 from time import sleep
 from Epaper import *
 from PIL import Image, UnidentifiedImageError
 import sys
+from copy import deepcopy
 
 def print_to_epaper(imgfile, frequency=30):
     file_error_counter = 0 #FileNotFound or unidentified image error counter
@@ -12,20 +14,30 @@ def print_to_epaper(imgfile, frequency=30):
     while True: 
         try:
             e = Epaper(X_PIXEL, Y_PIXEL)
-            with Image.open(imgfile) as f:
-                file_error_counter = 0
-                f = f.rotate(angle=90, expand=True)
 
-                rBuf = [0] * 4000
-                bBuf = [0] * 4000
+            #This causes an OSError: "too many open files" due to a bug in the PIL library. See Stackoverflow question #29234413. Workaround below.
+            # with Image.open(imgfile) as f:
+            #     file_error_counter = 0
+            #     f = f.rotate(angle=90, expand=True)
 
-                f = f.convert('RGB')
-                data = f.load()
-                f.close() #Should already be handled by with statement, but getting "OSError: too many open files" without this for some reason
+            #     rBuf = [0] * 4000
+            #     bBuf = [0] * 4000
+
+            #     f = f.convert('RGB')
+            #     data = f.load() 
+
+            imgfile = deepcopy(Image.open(imgfile))
+            file_error_counter = 0
+            imgfile = f.rotate(angle=90, expand=True)
+
+            rBuf = [0] * 4000
+            bBuf = [0] * 4000
+            
+            f = f.convert('RGB')
 
             for y in range(250): #This piece of code is directly taken from the geeekpi demo.py file
                 for x in range(128):
-                    if data[x,y] == (237,28,36): #IndexError: image index out of range TODO: Probably resize image to correct resolution.
+                    if data[x,y] == (237,28,36):
                         index = int(16 * y + (15 - (x - 7) / 8))
                         tmp = rBuf[index]
                         rBuf[index] = tmp | (1 << (x % 8))
